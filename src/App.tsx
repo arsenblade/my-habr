a5f5d092d65fb4f83c88cbac1a045f85ad8b1202
Arsen <arsenblade@mail.ru>
2023-02-04 20:50:53 +0500

    Организация стилей. Добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import './index.scss'
+import React, { useContext, useState } from 'react'
+import './styles/index.scss'
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={() => toggleTheme()}>Toggle</button>
       <h1>gbndbd</h1>
     </div>
   )

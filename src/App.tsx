80ff936e92415844ed3531afca86abbc64d4f3f4
Arsen <arsenblade@mail.ru>
2023-02-04 21:05:29 +0500

    Добавление хелпера classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react'
+import { classNames } from './helpers/classNames/classNames';
 import './styles/index.scss'
 import { Theme, ThemeContext } from './theme/ThemeContext';
 import { useTheme } from './theme/useTheme';
@@ -7,7 +8,7 @@ const App = () => {
   const {theme, toggleTheme} = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={() => toggleTheme()}>Toggle</button>
       <h1>gbndbd</h1>
     </div>

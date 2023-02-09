be56ce7544b6ca7fc0e62403a8bcb7ad4be27714
Arsen <arsenblade@mail.ru>
2023-02-09 22:37:45 +0500

    Svg loader. File loader. Button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,12 +8,11 @@ import { Navbar } from 'widgets/Navbar';
 
 
 const App = () => {
-  const {theme, toggleTheme} = useTheme()
+  const {theme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <button onClick={() => toggleTheme()}>Toggle</button>
       <h1>gbndbd</h1>
       <AppRouter />
     </div>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,6 +11,7 @@ interface NavbarProps {
 export const Navbar:FC<NavbarProps> = ({className}) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>Главная</AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О нас</AppLink>
